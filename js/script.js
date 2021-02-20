f1abfeb4f28bea3ce85d3b145bcff7e79d5f3b8d
rostyslav <martynykrostyslaw@gmail.com>
2021-02-20 20:35:56 +0200

    practice p.2

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
 "use strict";
 
-let num = 50;
+const numberOfFilms = +prompt('How many films have you watched?', '');
 
-while (num <= 55) {
-    console.log(num);
-    num++;
-}
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
 
-do {
-    console.log(num);
-    num++;
-}
-while (num < 55);
 
-for (let i = 1; i < 8; i++) {
-    if (i == 6) {
-        continue;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('What movie did you watch last?', ''),
+          b = prompt('Rate it', '');
+
+    if (a != null && b != null && a != '' && b !='' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
     }
-    console.log(i);
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Small amount of films');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Standart user');
+} else if (personalMovieDB.count >= 30 ) {
+    console.log('Fan!');
+} else {
+    console.log ('Error!');
 }
