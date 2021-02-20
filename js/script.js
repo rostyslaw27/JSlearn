15f42abaf2dc6b098d03cd5f616ca293401397e1
rostyslav <martynykrostyslaw@gmail.com>
2021-02-20 16:34:55 +0200

    variables and use strict

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+obj.a = 10;
+console.log(obj);
+
+console.log(name);
+var name = 'Ivan';
+
+{
+    let result = 50;
+}
+console.log(result);
