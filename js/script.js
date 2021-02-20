60957837cfdccf7d4dd780704f6d972e0dfab785
rostyslav <martynykrostyslaw@gmail.com>
2021-02-20 20:13:36 +0200

    cycles

--- js/script.js
+++ js/script.js
@@ -1,34 +1,21 @@
 "use strict";
 
-if(4 == 4) {
-    console.log('Ok!');
-} else {
-    console.log('Error!');
+let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
 }
 
-const num = 50;
-
-if (num < 49) {
-    console.log('Error!');
-} else if (num > 100) {
-    console.log('Many!');
-} else {
-    console.log('Ok!');
+do {
+    console.log(num);
+    num++;
 }
+while (num < 55);
 
-(num === 50) ? console.log('Ok!') : console.log('Error');
-
-switch (num) {
-    case 49: 
-        console.log('False');
-        break;
-    case 100:
-        console.log('False');
-        break;
-    case 50:
-        console.log('True');
-        break;
-    default:
-        console.log('Not this time');
-        break;
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
 }
