2c3ce1445da2fa3c93b7d724c6a8a18d873323d6
rostyslav <martynykrostyslaw@gmail.com>
2021-02-21 16:21:42 +0200

    OOP basics

--- js/script.js
+++ js/script.js
@@ -1,81 +1,25 @@
 "use strict";
 
-const obj = {
-    a: 5,
-    b: 1
+let str = 'some';
+let strObj = new String(str);
+
+console.log(typeof(str));
+console.log(typeof(strObj));
+
+const soldier = {
+    health: 400,
+    armor: 100
 };
 
-const copy1 = obj; // передача по посиланню
-
-copy1.a = 10;
-
-console.log(copy1);
-console.log(obj);
-
-function copy(mainObj) { //поверхнє копіювання об'єкта 
-    let objCopy = {};
-
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
+const john = {
+    health: 100
 };
 
-const newNumbers = copy(numbers);
+john.__proto__ = soldier; // не використовувати
 
-newNumbers.a = 10;
-newNumbers.c.x = 10; //поверхнісні копії
+console.log(john);
 
-console.log(newNumbers);
-console.log(numbers);
+console.log(john.armor);
 
-const add = {
-    d: 17,
-    e: 20
-};
-
-console.log(Object.assign(numbers, add)); //додавання об'єктів
-console.log(Object.assign({}, add));
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice(); //копіювання масиву
-
-
-//SPREAD OPERATOR - ОПЕРАТОР РОЗВОРОТУ
-const video = [ 'youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'livejournal', 'blogger'],
-    internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-const array = ['a', 'b'];
-
-const newAarray = [...array];
-
-const q = {
-    one: 1,
-    two: 2
-};
-
-const newObj = {...q};
+Object.setPrototypeOf(john, soldier); //правильне використання
+const john1 = Object.create(soldier);
