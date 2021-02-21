36abb13bc75a9bc329ea51afd0586072457b9eb0
rostyslav <martynykrostyslaw@gmail.com>
2021-02-21 14:46:49 +0200

    objects and destructuring

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
 "use strict";
 
-function learnJS(lang, callback) {
-    console.log(`I'm learning ${lang}`);
-    callback();
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
 
-function done() {
-    console.log('I finished this lesson');
-}
+options.makeTest();
 
-learnJS('JavaScript', done);
+const {border, bg} = options.colors;
+console.log(border);
+
+console.log(options["colors"]["border"]);
+
+delete options.name;
+
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Property ${i} have value ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Property ${key} have value ${options[key]}`);
+        counter++;
+    }
+}
+console.log(counter);
+
+console.log(Object.keys(options).length);
