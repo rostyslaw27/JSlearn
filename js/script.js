05d25ad10d148dba26f5511e25b7d4238d86d620
rostyslav <martynykrostyslaw@gmail.com>
2021-02-21 16:09:18 +0200

    spread operator

--- js/script.js
+++ js/script.js
@@ -1,31 +1,81 @@
 "use strict";
 
-const arr = [1, 2, 3, 6, 8];
+const obj = {
+    a: 5,
+    b: 1
+};
 
-arr.pop();
-arr.push(10);
+const copy1 = obj; // передача по посиланню
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+copy1.a = 10;
+
+console.log(copy1);
+console.log(obj);
+
+function copy(mainObj) { //поверхнє копіювання об'єкта 
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
 }
 
-for (let value of arr) {
-    console.log(value);
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10; //поверхнісні копії
+
+console.log(newNumbers);
+console.log(numbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+console.log(Object.assign(numbers, add)); //додавання об'єктів
+console.log(Object.assign({}, add));
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice(); //копіювання масиву
+
+
+//SPREAD OPERATOR - ОПЕРАТОР РОЗВОРОТУ
+const video = [ 'youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'livejournal', 'blogger'],
+    internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
 
-arr.forEach(function(item, i, arr) {
-    console.log(`${i}: ${item} in array ${arr}`);
-});
+const num = [2, 5, 7];
 
-const str = prompt('', '');
-const products = str.split(', ');
-products.sort();
-console.log(products.join('; '));
+log(...num);
 
-const arr2 = [2, 13, 26, 8, 10];
+const array = ['a', 'b'];
 
-arr2.sort(compareNum);
+const newAarray = [...array];
 
-function compareNum(a, b) {
-    return a-b;
-}
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
