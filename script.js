558249ca3408ef8f01735a9cb23588500804f76f
rostyslav <martynykrostyslaw@gmail.com>
2021-02-22 17:26:28 +0200

    DOM navigation

--- script.js
+++ script.js
@@ -1,50 +1,15 @@
-"use strict";
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.body.childNodes);
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
 
-//1)
-//<button onclick="alert('Click')" id="btn"></button> //не використовується
+console.log(document.querySelector('#current').parentNode.parentNode);
 
-//2)
-const btn = document.querySelector('button'),
-      overlay = document.querySelector('.overlay'),
-      btns = document.querySelectorAll('button');
+for (let node of document.body.clildNodes) {
 
-//btn.onclick = function() {
- //   alert('Click');
-//}; // не використовується
-
-//3)
-
-btn.addEventListener('click', () => {
-    alert('Click');
-});
-
-btn.addEventListener('click', () => {
-    alert('Second click');
-});
-
-let i = 0;
-const deleteElement = (e) => {
-    console.log(e.target);
-    console.log(e.type);
-    //e.target.remove();
-    // i++;
-    // if (i == 1) {
-    //     btn.removeEventListener('mouseenter', deleteElement);
-    // }
-};
-
-btn.addEventListener('mouseenter', deleteElement);
-overlay.addEventListener('mouseenter', deleteElement);
-
-// вспливання подій - подія відбуваєтья спершу на вложеному
-
-const link = document.querySelector('a');
-link.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    console.log(event.target);
-});
-
-btns.forEach(btn => {
-    btn.addEventListener('click', deleteElement, {once: true});
-});
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    console.log(node);
+}
