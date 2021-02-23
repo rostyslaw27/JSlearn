78f688dc7e6ec8296f5b9654baae0c737af96953
rostyslav <martynykrostyslaw@gmail.com>
2021-02-23 14:57:25 +0200

    tabs

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-const btns = document.querySelectorAll('button'),
-      wrapper = document.querySelector('.btn-block');
+window.addEventListener('DOMContentLoaded', () => {
 
-console.log(btns[0].classList.length); // кількість класів в кнопки
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
 
-console.log(btns[0].classList.item(0)); //перший клас
+    
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            //item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
 
-console.log(btns[0].classList.add('red', 'green')); // додавання класу
-
-console.log(btns[0].classList.remove('blue'));
-
-console.log(btns[0].classList.toggle('blue')); // якщо є - видаляє, якщо немає - додає
-
-if (btns[1].classList.contains('red')) {
-    console.log('red');
-}
-
-btns[0].addEventListener('click', () => {
-    if(!btns[1].classList.contains('red')) {
-        btns[1].classList.add('red');
-    } else {
-        btns[1].classList.remove('red');
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
     }
 
-    btns[1].classList.toggle('red');
+    function showTabContent(i = 0) {
+        //tabsContent[i].style.display = 'block';
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent(0);
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
 });
-
-console.log(btns[0].className);
-
-wrapper.addEventListener('click', (event) => {
-    if (event.target && event.target.tagName == 'BUTTON' && event.target.matches('button.red')) {
-        console.log('Hello');
-    }
-}); // делегування
-
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn);
