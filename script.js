cbf997e514b3c29b066f0df5fbe2414421fa26dd
rostyslav <martynykrostyslaw@gmail.com>
2021-02-28 20:11:18 +0200

    this

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 "use strict";
 
+function showThis(a, b) {
+    console.log(this);
+    function sum() {
+        console.log(this);
+        return a + b;
+    }
+    console.log(sum());
+}
+showThis(4, 5);
+
+
+const obj ={
+    a: 20,
+    b: 15,
+    sum: function() {
+        console.log(this);
+    }
+};
+obj.sum();
+
+
 function User(name, id) {
     this.name = name;
     this.id = id;
@@ -9,13 +30,47 @@ function User(name, id) {
     };
 }
 
-User.prototype.exit = function() {
-    console.log(`${this.name} gone`);
+
+function sayName(surname) {
+    console.log(this);
+    console.log(this.name + surname);
+}
+const user = {
+    name: 'John'
+};
+sayName.call(user, 'Smith');
+sayName.apply(user, ['Smith']);
+
+
+function count(num) {
+    return this*num;
+}
+const double = count.bind(2);
+console.log(double(3));
+
+//1) this = window, use strict = undefined
+//2) Контекст у методів об'єкта буде сам об'єкт
+//3) this в конструкторах і класах - новий екземпляр об'єкта
+//4) Ручна прив'язка this: call, apply, bind
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    console.log(this); // контекстом є сам елемент
+    this.style.backgroundColor = 'red';
+});
+
+const obj1 = {
+    num: 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this); // стрілочна функція не має контексту виклику, вона бере його у батька
+        };
+
+        say();
+    }
 };
 
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 20);
+obj.sayNumber();
 
-ivan.exit();
-
-ivan.hello();
+const double11 = a => a * 2;
