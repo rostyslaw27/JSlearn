294b62d46cefe42b057077e8f4922af8d165d90e
rostyslav <martynykrostyslaw@gmail.com>
2021-03-05 22:29:54 +0200

    AJAX/XMLHttpRequest

--- js/script.js
+++ js/script.js
@@ -1,50 +1,38 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
 
-const adv = document.querySelectorAll('.promo__adv img'),
-      poster = document.querySelector('.promo__bg'),
-      genre = poster.querySelector('.promo__genre'),
-      movieList = document.querySelector('.promo__interactive-list');
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
 
-adv.forEach(item => {
-    item.remove();
-});
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
 
-genre.textContent = 'драма';
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = 'Error';
+        }
+    });
 
-poster.style.backgroundImage = 'url("img/bg.jpg")';
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = 'Error';
+        }
+    });
+    //status 404
+    //statusText Not Found
+    //response
+    //readyState
 
-movieList.innerHTML = '';
 
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
 });
