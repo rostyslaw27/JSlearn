86de8220b60a574985ea6ab65121de179a65ae28
rostyslav <martynykrostyslaw@gmail.com>
2021-04-12 12:36:25 +0300

    incapsulation

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 'use strict';
 
-const persone = {
-    name: 'Alex',
-    age: 25,
-
-    get userAge() {
-        return this.age;
-    },
-
-    set userAge(num) {
-        this.age = num;
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
     }
-};
 
-console.log(persone.userAge = 30);
-console.log(persone.userAge);
+    #surname = 'Mos';
+
+    say () {
+        console.log(`User name: ${this.name} ${this.surname}, age ${this._age}`);
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age (age) {
+        if (typeof(age) === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        }  else {
+            console.log('Unexceptionable value');
+        }
+    }
+
+    get surname() {
+        return this.#surname;
+    }
+     
+    set surname(surname) {
+        this.#surname = surname;
+    }
+}
+
+const ivan = new User('Ivan', 27);
+console.log(ivan.surname);
+ivan.say();
+ivan.surname = 'Smith';
+console.log(ivan.surname);  
