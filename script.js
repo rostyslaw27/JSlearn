fe76870797a2de31f6b77fcb612afbb4ab3bc372
rostyslav <martynykrostyslaw@gmail.com>
2021-04-12 12:59:20 +0300

    modules

--- script.js
+++ script.js
@@ -1,40 +1,25 @@
 'use strict';
 
-class User {
-    constructor(name, age) {
-        this.name = name;
-        this._age = age;
-    }
+const number = 1;
 
-    #surname = 'Mos';
+// 1
+(function() {
+    let number = 2;
+    console.log(number);
+    console.log(number + 3);
+}());
 
-    say () {
-        console.log(`User name: ${this.name} ${this.surname}, age ${this._age}`);
-    }
+console.log(number);
 
-    get age() {
-        return this._age;
-    }
+// 2
+const user = (function(){
+    const privat = function() {
+        console.log('I am privat!');
+    };
 
-    set age (age) {
-        if (typeof(age) === 'number' && age > 0 && age < 110) {
-            this._age = age;
-        }  else {
-            console.log('Unexceptionable value');
-        }
-    }
+    return {
+        sayHello: privat
+    };
+}());
 
-    get surname() {
-        return this.#surname;
-    }
-     
-    set surname(surname) {
-        this.#surname = surname;
-    }
-}
-
-const ivan = new User('Ivan', 27);
-console.log(ivan.surname);
-ivan.say();
-ivan.surname = 'Smith';
-console.log(ivan.surname);  
+user.sayHello();
