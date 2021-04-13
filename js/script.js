9ab4fd143973304d607d59ce94193d3591deed68
rostyslav <martynykrostyslaw@gmail.com>
2021-04-13 12:25:25 +0300

    babel/core.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
-'use strict';
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
 
-try {
-    console.log('Normal');
-    console.log(a);
-} catch(e) {
-    console.log(e.name);
-    console.log(e.message);
-    console.log(e.stack);
-} finally {
-    
-}
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import {openModal} from './modules/modal';
 
-console.log('After error');
+window.addEventListener('DOMContentLoaded', () => {
+    const modalTimerID = setTimeout(() => openModal('.modal', modalTimerID), 500000); // super trick
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerID);
+    timer('.timer', '2021-06-01');
+    cards();
+    calc();
+    forms('form', modalTimerID);
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        slide: '.offer__slide',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+}); 
