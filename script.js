01315c7d6d945550440063964a2b95c771d053e1
rostyslav <martynykrostyslaw@gmail.com>
2021-04-14 23:59:03 +0300

    requestAnimationFrame

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-'use strict';
+const btn = document.querySelector('.btn'),
+      elem = document.querySelector('.box');  
+let pos = 0;
 
-function* generator() {
-    yield 'S';
-    yield 'C';
-    yield 'r';
-    yield 'p';
-    yield 't';
-}
+// function myAnimation() {
+//     let pos = 0;
 
-const str = generator();
+//     const id = setInterval(frame, 10);
+//     function frame() {
+//         if (pos == 300) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             elem.style.top = pos + "px";
+//             elem.style.left = pos + 'px';
+//         }
+//     }
+// }
 
-console.log(str.next());
-console.log(str.next().value);
-console.log(str.next());
+function myAnimation() {
+    pos++;
+    elem.style.top = pos + "px";
+    elem.style.left = pos + 'px';
 
-function* count(n) {
-    for (let i = 0; i < n; i++) {
-        yield i;
+    if (pos < 300) {
+        requestAnimationFrame(myAnimation);
     }
 }
 
-for (let k of count(7)) {
-    console.log(k);
-}
+btn.addEventListener('click', () => requestAnimationFrame(myAnimation));
+
+let id = requestAnimationFrame(myAnimation);
+cancelAnimationFrame(id);
